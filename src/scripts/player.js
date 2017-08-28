5f160599ae9f6d83b459976bc33b17267dd74d9d
Dorian Delorme <dorian.delorme@hetic.net>
2017-08-28 11:12:40 +0200

    Move video in JS

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,1 +1,15 @@
 console.log('hello world');
+
+let container = document.querySelector('.container');
+
+let video = document.createElement('video');
+
+video.src = "./src/videos/video.mp4";
+
+video.id = "video";
+
+video.autoplay = "true";
+
+video.controls = "true";
+
+container.appendChild(video);
