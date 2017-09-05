dfccae2113d96de7ae75e8ff5b6cf6e8b87ecb6a
Dorian Delorme <dorian.delorme@hetic.net>
2017-09-05 17:17:31 +0200

    Fix

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,15 +1,15 @@
-console.log('hello world');
+console.log('hello world')
 
-let container = document.querySelector('.container');
+let container = document.querySelector('.container')
 
-let video = document.createElement('video');
+let video = document.createElement('video')
 
-video.src = "./src/videos/video.mp4";
+video.src = "./src/videos/video.mp4"
 
-video.id = "video";
+video.id = "video"
 
-video.autoplay = "true";
+video.autoplay = "true"
 
-video.controls = "true";
+video.controls = "true"
 
-container.appendChild(video);
+container.appendChild(video)
