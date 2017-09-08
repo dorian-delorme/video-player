786148c48a0915e856dcd241241b9a5c99685e78
Dorian Delorme <dorian.delorme@hetic.net>
2017-09-08 17:59:57 +0200

    Add play pause event

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,5 +1,7 @@
 // Beautiful player using POO
 
+// Work In Progress
+
 class Player {
   constructor(basePath, parent, id, className, link, width, height, autoplay) {
     this.parent    = document.querySelector(parent)
@@ -10,7 +12,7 @@ class Player {
     this.id        = id
     this.className = className
   }
-  
+
   create() {
     let myPlayer = document.createElement('video')
 
@@ -23,8 +25,58 @@ class Player {
 
     this.parent.appendChild(myPlayer)
   }
+
+  controls() {
+    // Target the player
+    let player = document.getElementById(this.id)
+
+    // Create control bar
+    let controlBar = document.createElement('div')
+    controlBar.className = 'controlBar'
+    this.parent.appendChild(controlBar)
+
+    // Create play/pause button
+    let playPauseButton = document.createElement('div')
+    playPauseButton.className = 'playPauseButton'
+    controlBar.appendChild(playPauseButton)
+
+    // Check Video Status to display the right button
+    // if(player.paused) {
+    //   player.className.remove('.isPlaying')
+    //   player.className.add('.isPaused')
+    // }
+    // else {
+    //   player.className.remove('.isPaused')
+    //   player.className.add('.isPlaying')
+    // }
+
+    // Event Play/Pause on click video
+    player.addEventListener('click', function() {
+      if(player.paused) {
+        player.play()
+      }
+      else {
+        player.pause()
+      }
+    })
+
+    // Event Play/Pause on click button
+    playPauseButton.addEventListener('click', function() {
+      if(player.paused) {
+        player.play()
+      }
+      else {
+        player.pause()
+      }
+    })
+    console.log(player.currentTime);
+  }
 }
 
 // Creating new Player
 
-new Player('./src/videos/', '.container', '', 'playerCustom', 'video.mp4', 400, 300, true).create()
+let customPlayer = new Player('./src/videos/', '.player', '1', 'customPlayer', 'video.mp4', 400, 300, true)
+
+customPlayer.create()
+
+customPlayer.controls()
